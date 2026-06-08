49cb775b1d3cbbd4a877afe1d0f636bdb8c69392
Jay <jayranipa084@gmail.com>
2026-06-08 10:03:41 +0530

    Deploy frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/sidebar";
 
 function Dashboard() {
   const [isOpen, setIsOpen] = useState(false);
